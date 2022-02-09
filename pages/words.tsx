41a144231194f439e227d5a81d22a9b8c93c9e65
えむけー <mk@mktoho.org>
2022-02-09 13:45:19 +0900

    scrape words

---
+++ pages/words.tsx
@@ -0,0 +1,39 @@
+import { GetStaticProps } from "next"
+import * as puppeteer from "puppeteer"
+
+type Props = {
+  words: string[]
+}
+
+const Words = ({ words }: Props) => (
+  <ul>
+    {words.map((word) => (
+      <li>{word}</li>
+    ))}
+  </ul>
+)
+
+export default Words
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const browser = await puppeteer.launch()
+    const select = () => Array.from(document.querySelectorAll('.word-block ul li')).map(li => li.textContent)
+    const words = await Promise.all('abcdefghijklmnopqrstuvwxyz'.split('').map(letter => 
+      browser.newPage().then(page =>
+        page.goto(`https://www.wordmom.com/5-letter-words/that-start-with-${letter}`)
+            .then(() =>
+              page.evaluate(select)
+                  .then(words => {
+                    page.close()
+                    return words
+                  })
+            )
+      )
+    )).then(wordses => wordses.flat())
+    
+    return { props: { words } }
+  } catch (err: any) {
+    return { props: { errors: err.message } }
+  }
+}
