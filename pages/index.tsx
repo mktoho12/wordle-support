321afa2bb67317930432fdb9164bc88db3b5e5b1
mktoho <mk@mktoho.org>
2022-02-10 19:51:41 +0900

    mobile use

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from "next"
-import { useEffect, useState } from "react"
+import { MouseEvent, useEffect, useState } from "react"
 import wordListPath from "word-list"
 import { readFileSync } from "fs"
 import GameRow from "../components/GameRow"
@@ -30,8 +30,6 @@ const Home = ({ words, errors }: Props) => {
         return word => word.includes(letter) && word[index] !== letter
       case Evaluation.Absent:
         return word => !word.includes(letter)
-      default:
-        return _ => false
     }
   }
   
@@ -39,6 +37,15 @@ const Home = ({ words, errors }: Props) => {
     () => setCandidates(inputs.flat().map(predicate).reduce((accm, p) => accm.filter(p), words)),
     [inputs, words]
   )
+
+  const keyPress = (event: MouseEvent<HTMLButtonElement>) => {
+    let key = event.currentTarget.dataset.key
+    if(!key) return;
+
+    document.dispatchEvent(new KeyboardEvent("keydown", { key: key === "←" ? "Backspace" : key }))
+  }
+
+  const renderKey = (key: string) => <button data-key={key} onClick={keyPress} key={key}>{key}</button>
 
   return <>
     <Head>
@@ -95,6 +102,25 @@ const Home = ({ words, errors }: Props) => {
         </div>
       </div>
       <footer>
+        <div id="keyboard">
+          <div className="row">
+            {"qwertyuiop".split("").map(renderKey)}
+          </div>
+          <div className="row">
+            <div className="spacer half"></div>
+            {"asdfghjkl".split("").map(renderKey)}
+            <div className="spacer half"></div>
+          </div>
+          <div className="row">
+            <div className="spacer one-and-a-half"></div>
+            {"zxcvbnm".split("").map(renderKey)}
+            <button data-key="←" className="one-and-a-half" onClick={keyPress}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                <path fill="var(--color-tone-1)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
         <p>Type keyboard and click letter.</p>
       </footer>
       <div className="github">
